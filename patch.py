1eae84ea62542f8adc4120169471e901529ec1f2
Prathamesh Pathak <40099710+Sam-Bonfire@users.noreply.github.com>
2026-08-25 17:22:45 +0530

    Delete patch.py

--- patch.py
+++
@@ -1,7 +0,0 @@
-with open("frontend-calendar/src/routes/+page.svelte", "r") as f:
-    data = f.read()
-
-data = data.replace("{:else if isDetailsDocked && !isMobileOrTablet}", "{:else if isDetailsDocked && !isMobileOrTablet && !selectedEvent}")
-
-with open("frontend-calendar/src/routes/+page.svelte", "w") as f:
-    f.write(data)
