760a8c94cf1931651272a4d878ddc93823a91628
Prathamesh Pathak <40099710+Sam-Bonfire@users.noreply.github.com>
2026-08-25 17:23:23 +0530

    Delete patch3.py

--- patch3.py
+++
@@ -1,8 +0,0 @@
-with open("frontend-calendar/src/routes/+page.svelte", "r") as f:
-    data = f.read()
-
-# Fix the spacing
-data = data.replace("{isDetailsDocked && !clickPosition ? 'lg:mr-80' : ''}", "{isDetailsDocked ? 'lg:mr-80' : ''}")
-
-with open("frontend-calendar/src/routes/+page.svelte", "w") as f:
-    f.write(data)
